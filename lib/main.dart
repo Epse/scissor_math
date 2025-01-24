0762505ed313f1379d3a90fd93b875f9a601d608
Stef Pletinck <s@stefpletinck.be>
2025-01-24 11:04:41 +0100

    Add Lutterloh layou

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
@@ -35,23 +35,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double correction = 0.0;
+  double result = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(AppLocalizations.of(context)!.appTitle),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(AppLocalizations.of(context)!.appTitle),
         ),
-      ),
-    );
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(spacing: 16.0, children: [
+            Text(AppLocalizations.of(context)!.lSystem,
+                style: Theme.of(context).textTheme.titleLarge),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: AppLocalizations.of(context)!.measurementHipBust,
+              ),
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            ),
+            Text(AppLocalizations.of(context)!
+                .correction(correction.toString(), "cm")),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: AppLocalizations.of(context)!.patternRadius,
+              ),
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            ),
+            Text(AppLocalizations.of(context)!.result("cm", result.toString()),
+                style: Theme.of(context).textTheme.headlineMedium)
+          ]),
+        ));
   }
 }
