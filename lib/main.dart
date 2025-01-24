aecaafbddff9a35fb0f8b06a008fab0f9a80563f
Stef Pletinck <s@stefpletinck.be>
2025-01-24 14:50:15 +0100

    Added support for navigation in the future

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:scissor_math/pages/lutterloh.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,81 +22,10 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const MyHomePage(),
+      routes: {
+        "/": (context) => const LutterlohScreen(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  double measurement = 0.0;
-  double radius = 0.0;
-
-  double _lutterlohCorrection(double measurement) {
-    return 7 * (measurement - 96.0) / 46.0;
-  }
-
-  double _lutterloh(double measurement, double radius) {
-    return radius + _lutterlohCorrection(measurement);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var theme = Theme.of(context).colorScheme;
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(AppLocalizations.of(context)!.appTitle),
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: ListView(children: [
-            Text(AppLocalizations.of(context)!.lSystem,
-                style: Theme.of(context).textTheme.headlineLarge),
-            Padding(padding: EdgeInsets.all(32.0)),
-            TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: AppLocalizations.of(context)!.measurementHipBust,
-                ),
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                onChanged: (String input) {
-                  setState(() {
-                    measurement = double.tryParse(input) ?? 0;
-                  });
-                }),
-            Padding(padding: EdgeInsets.all(16.0)),
-            Text(
-                AppLocalizations.of(context)!.correction(
-                    _lutterlohCorrection(measurement).toStringAsFixed(1), "cm"),
-                style: Theme.of(context).textTheme.titleMedium),
-            Padding(padding: EdgeInsets.all(16.0)),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: AppLocalizations.of(context)!.patternRadius,
-              ),
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              onChanged: (String input) {
-                setState(() {
-                  radius = double.tryParse(input) ?? 0;
-                });
-              },
-            ),
-            Padding(padding: EdgeInsets.all(16.0)),
-            Text(
-                AppLocalizations.of(context)!.result(
-                    "cm", _lutterloh(measurement, radius).toStringAsFixed(1)),
-                style: Theme.of(context).textTheme.titleLarge)
-          ]),
-        ));
-  }
-}
---
+++ lib/pages/lutterloh.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class LutterlohScreen extends StatefulWidget {
+  const LutterlohScreen({super.key});
+
+  @override
+  State<LutterlohScreen> createState() => _LutterlohScreenState();
+}
+
+class _LutterlohScreenState extends State<LutterlohScreen> {
+  double measurement = 0.0;
+  double radius = 0.0;
+
+  double _lutterlohCorrection(double measurement) {
+    return 7 * (measurement - 96.0) / 46.0;
+  }
+
+  double _lutterloh(double measurement, double radius) {
+    return radius + _lutterlohCorrection(measurement);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context).colorScheme;
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(AppLocalizations.of(context)!.appTitle),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: ListView(children: [
+            Text(AppLocalizations.of(context)!.lSystem,
+                style: Theme.of(context).textTheme.headlineLarge),
+            Padding(padding: EdgeInsets.all(32.0)),
+            TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: AppLocalizations.of(context)!.measurementHipBust,
+                ),
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                onChanged: (String input) {
+                  setState(() {
+                    measurement = double.tryParse(input) ?? 0;
+                  });
+                }),
+            Padding(padding: EdgeInsets.all(16.0)),
+            Text(
+                AppLocalizations.of(context)!.correction(
+                    _lutterlohCorrection(measurement).toStringAsFixed(1), "cm"),
+                style: Theme.of(context).textTheme.titleMedium),
+            Padding(padding: EdgeInsets.all(16.0)),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: AppLocalizations.of(context)!.patternRadius,
+              ),
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              onChanged: (String input) {
+                setState(() {
+                  radius = double.tryParse(input) ?? 0;
+                });
+              },
+            ),
+            Padding(padding: EdgeInsets.all(16.0)),
+            Text(
+                AppLocalizations.of(context)!.result(
+                    "cm", _lutterloh(measurement, radius).toStringAsFixed(1)),
+                style: Theme.of(context).textTheme.titleLarge)
+          ]),
+        ));
+  }
+}
